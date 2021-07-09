dda0452919d0e382c3da8073e6935645e5d20c30
Eugenio Paolantonio (g7) <me@medesimo.eu>
2021-07-09 23:31:21 +0200

    [android-system-gsi-29-bin] download: pick up artifact from the halium-10.0 branch
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

--- download.sh
+++ download.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-CI_JOB_ID="lastSuccessfulBuild"
+CI_JOB_ID="215"
+BRANCH="halium-10.0"
 ARCH="arm64"
 
-DOWNLOAD_URL="https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%252Fcommunity-ports%252Fjenkins-ci%252Fgeneric_${ARCH}/job/main/${CI_JOB_ID}/artifact/halium_halium_${ARCH}.tar.xz"
+DOWNLOAD_URL="https://ci.ubports.com/job/UBportsCommunityPortsJenkinsCI/job/ubports%252Fcommunity-ports%252Fjenkins-ci%252Fgeneric_${ARCH}/job/${BRANCH}/${CI_JOB_ID}/artifact/halium_halium_${ARCH}.tar.xz"
 
 rm -rf downloaded_artifacts
 mkdir -p downloaded_artifacts
